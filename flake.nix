4a15af566b146186de4bad850e04f3856e2a9b18
drzzln <drzzln@protonmail.com>
2026-03-15 19:56:35 -0400

    add nix flake with substrate integration

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "Akeyless C# SDK - auto-generated .NET client for the Akeyless API";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    substrate = { url = "github:pleme-io/substrate"; inputs.nixpkgs.follows = "nixpkgs"; };
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, substrate, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.default = pkgs.mkShellNoCC {
+        packages = with pkgs; [ dotnet-sdk_8 ];
+      };
+    });
+}
