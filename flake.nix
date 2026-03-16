43153b1d89aa8b22ac963f0e968a07d0a4dcad10
drzzln <drzzln@protonmail.com>
2026-03-15 20:55:07 -0400

    fix flake.nix structure and add flake.lock

--- flake.nix
+++ flake.nix
@@ -1,7 +1,11 @@
 {
   description = "Akeyless C# SDK - auto-generated .NET client for the Akeyless API";
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = import nixpkgs { inherit system; };
